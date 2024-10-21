210a262aadf78bce376354caaa2b3852258bd8a7
Derek Ashby <dbashby@byu.edu>
2024-10-21 11:33:34 -0600

    Established and tested DataAccess classes

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,34 @@
+package dataaccess;
+
+import model.*;
+import java.util.ArrayList;
+
+public interface DataAccess {
+    UserData findUserData(String username);
+
+    void createUser(String username, UserData data);
+
+    AuthData findAuthData(String authToken);
+
+    void createAuth(String authToken, AuthData data);
+
+    ArrayList<GameData> listGames();
+
+    GameData findGameData(Integer gameID);
+
+    void createGame(Integer gameID, GameData data);
+
+    void updateGame(Integer gameID, GameData data);
+
+    void clearUserDAO();
+
+    void clearAuthDAO();
+
+    void clearGameDAO();
+
+    boolean isUserDataEmpty();
+
+    boolean isAuthDataEmpty();
+
+    boolean isGameDataEmpty();
+}
