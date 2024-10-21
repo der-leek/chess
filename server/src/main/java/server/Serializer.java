d43086d4c1eeddd76d8a1ffcce857c5556000bbc
Derek Ashby <dbashby@byu.edu>
2024-10-21 12:28:54 -0600

    Clear endpoint passes all tests!

---
+++ server/src/main/java/server/Serializer.java
@@ -0,0 +1,19 @@
+package server;
+
+import com.google.gson.*;
+
+public class Serializer<T> {
+    private final Gson gson;
+
+    public Serializer() {
+        this.gson = new Gson();
+    }
+
+    public String toJson(T obj) {
+        return gson.toJson(obj);
+    }
+
+    public T fromJson(String json, Class<T> clazz) throws JsonSyntaxException {
+        return gson.fromJson(json, clazz);
+    }
+}
