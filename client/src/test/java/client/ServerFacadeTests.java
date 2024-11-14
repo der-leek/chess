126dcdd7c90d872cb2a0d66af286fdafdc151d4f
Derek Ashby <dbashby@byu.edu>
2024-11-13 21:52:57 -0700

    Join game has entered the chat 🫡

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -14,10 +14,12 @@ import com.google.gson.reflect.TypeToken;
 
 public class ServerFacadeTests {
 
+    private Double gameID;
     private String authToken;
     private static Server server;
     private static ServerFacade sf;
     private final Serializer serializer = new Serializer();
+    private final String gameName = "new game";
     private final String username = "der_leek";
     private final String password = "23Der!";
     private final String email = "der@mail.com";
@@ -41,10 +43,18 @@ public class ServerFacadeTests {
     @BeforeEach
     public void init() {
         sf.clear();
+
         var registerResult = sf.register(username, password, email);
-        var body = serializer.fromJson(registerResult.get("body"), Map.class);
-        authToken = (String) body.get("authToken");
+        var registerBody = serializer.fromJson(registerResult.get("body"), Map.class);
+        authToken = (String) registerBody.get("authToken");
+
         Assertions.assertNotNull(authToken);
+
+        Map<String, String> createResult = sf.createGame(gameName, authToken);
+        var createBody = serializer.fromJson(createResult.get("body"), Map.class);
+        gameID = (Double) createBody.get("gameID");
+
+        Assertions.assertNotNull(gameID);
     }
 
     @Test
@@ -58,17 +68,18 @@ public class ServerFacadeTests {
     @Test
     public void registerSuccess() {
         Map<String, String> result = sf.register("new_username", password, email);
-
         Assertions.assertNotNull(result);
         Assertions.assertEquals("200", result.get("statusCode"));
 
         var body = serializer.fromJson(result.get("body"), Map.class);
+
         Assertions.assertNotNull(body.get("authToken"));
     }
 
     @Test
     public void registerUsernameTaken() {
         Map<String, String> duplicate = sf.register(username, password, email);
+
         Assertions.assertNotNull(duplicate);
         Assertions.assertEquals("403", duplicate.get("statusCode"));
     }
@@ -76,6 +87,7 @@ public class ServerFacadeTests {
     @Test
     public void logoutSuccess() {
         Map<String, String> logoutResult = sf.logout(authToken);
+
         Assertions.assertNotNull(logoutResult);
         Assertions.assertEquals("200", logoutResult.get("statusCode"));
     }
@@ -83,6 +95,7 @@ public class ServerFacadeTests {
     @Test
     public void logoutBadAuth() {
         Map<String, String> logoutResult = sf.logout("badAuth");
+
         Assertions.assertNotNull(logoutResult);
         Assertions.assertEquals("401", logoutResult.get("statusCode"));
     }
@@ -90,6 +103,7 @@ public class ServerFacadeTests {
     @Test
     public void logoutNullAuth() {
         Map<String, String> logoutResult = sf.logout(null);
+
         Assertions.assertNotNull(logoutResult);
         Assertions.assertEquals("401", logoutResult.get("statusCode"));
     }
@@ -97,8 +111,8 @@ public class ServerFacadeTests {
     @Test
     public void loginSuccess() {
         sf.logout(authToken);
-
         Map<String, String> loginResult = sf.login(username, password);
+
         Assertions.assertNotNull(loginResult);
         Assertions.assertEquals("200", loginResult.get("statusCode"));
     }
@@ -106,8 +120,8 @@ public class ServerFacadeTests {
     @Test
     public void loginInvalidUsername() {
         sf.logout(authToken);
-
         Map<String, String> loginResult = sf.login("bad_username", password);
+
         Assertions.assertNotNull(loginResult);
         Assertions.assertEquals("401", loginResult.get("statusCode"));
     }
@@ -115,44 +129,36 @@ public class ServerFacadeTests {
     @Test
     public void loginInvalidPassword() {
         sf.logout(authToken);
-
         Map<String, String> loginResult = sf.login(username, "bad_password");
+
         Assertions.assertNotNull(loginResult);
         Assertions.assertEquals("401", loginResult.get("statusCode"));
     }
 
     @Test
     public void createGameSuccess() {
-        Map<String, String> createResult = sf.createGame("new game", authToken);
+        Map<String, String> createResult = sf.createGame("game", authToken);
         Assertions.assertNotNull(createResult);
         Assertions.assertEquals("200", createResult.get("statusCode"));
 
         var body = serializer.fromJson(createResult.get("body"), Map.class);
         var gameID = body.get("gameID");
+
         Assertions.assertInstanceOf(Double.class, gameID);
     }
 
     @Test
-    public void createTwoGames() {
-        Map<String, String> createResult = sf.createGame("new game", authToken);
-        Assertions.assertNotNull(createResult);
-        Assertions.assertEquals("200", createResult.get("statusCode"));
-
-        Map<String, String> newCreateResult = sf.createGame("another game", authToken);
-        Assertions.assertNotNull(newCreateResult);
-        Assertions.assertEquals("200", newCreateResult.get("statusCode"));
-    }
-
-    @Test
     public void createGameBadAuth() {
-        Map<String, String> createResult = sf.createGame("new game", "badAuth");
+        Map<String, String> createResult = sf.createGame("game", "badAuth");
+
         Assertions.assertNotNull(createResult);
         Assertions.assertEquals("401", createResult.get("statusCode"));
     }
 
     @Test
     public void createGameBadGameName() {
-        Map<String, String> createResult = sf.createGame("badName;", authToken);
+        Map<String, String> createResult = sf.createGame("bad; Name", authToken);
+
         Assertions.assertNotNull(createResult);
         Assertions.assertEquals("500", createResult.get("statusCode"));
     }
@@ -160,12 +166,13 @@ public class ServerFacadeTests {
     @Test
     public void listGamesBadAuth() {
         Map<String, String> listResult = sf.listGames("badAuth");
+
         Assertions.assertNotNull(listResult);
         Assertions.assertEquals("401", listResult.get("statusCode"));
     }
 
     @Test
-    public void listNoGames() {
+    public void listOneGame() {
         Map<String, String> listResult = sf.listGames(authToken);
         Assertions.assertNotNull(listResult);
         Assertions.assertEquals("200", listResult.get("statusCode"));
@@ -175,18 +182,15 @@ public class ServerFacadeTests {
         ArrayList<GameData> games = body.get("games");
 
         Assertions.assertNotNull(games);
-        Assertions.assertTrue(games.isEmpty());
+        Assertions.assertTrue(games.size() == 1);
     }
 
     @Test
     public void listTwoGames() {
         HashSet<String> gameNames = new HashSet<>();
         HashSet<Integer> gameIDs = new HashSet<>();
-
-        Map<String, String> createResult = sf.createGame("game1", authToken);
-        var createBody = serializer.fromJson(createResult.get("body"), Map.class);
-        gameIDs.add(((Double) createBody.get("gameID")).intValue());
-        gameNames.add("game1");
+        gameIDs.add(gameID.intValue());
+        gameNames.add(gameName);
 
         Map<String, String> newCreateResult = sf.createGame("game2", authToken);
         var newCreateBody = serializer.fromJson(newCreateResult.get("body"), Map.class);
@@ -198,8 +202,8 @@ public class ServerFacadeTests {
 
         Type type = new TypeToken<Map<String, ArrayList<GameData>>>() {}.getType();
         Map<String, ArrayList<GameData>> body = serializer.fromJson(listResult.get("body"), type);
-
         ArrayList<GameData> games = body.get("games");
+
         Assertions.assertNotNull(games);
         Assertions.assertFalse(games.isEmpty());
 
@@ -215,15 +219,35 @@ public class ServerFacadeTests {
 
     @Test
     public void joinGameSuccess() {
-        Map<String, String> createResult = sf.createGame("newGame", authToken);
-        var body = serializer.fromJson(createResult.get("body"), Map.class);
-        Double gameID = (Double) body.get("gameID");
-
         Map<String, String> joinResult =
                 sf.joinGame(Double.toString(gameID), ChessGame.TeamColor.WHITE, authToken);
 
         Assertions.assertNotNull(joinResult);
         Assertions.assertEquals("200", joinResult.get("statusCode"));
-        // add tests to verify that the game exists in the database
+
+        Map<String, String> listResult = sf.listGames(authToken);
+        Type type = new TypeToken<Map<String, ArrayList<GameData>>>() {}.getType();
+        Map<String, ArrayList<GameData>> listBody =
+                serializer.fromJson(listResult.get("body"), type);
+        GameData game = listBody.get("games").get(0);
+
+        Assertions.assertEquals(gameID, game.gameID());
+    }
+
+    @Test
+    public void joinGameDuplicateUsername() {
+        sf.joinGame(Double.toString(gameID), ChessGame.TeamColor.WHITE, authToken);
+        Map<String, String> joinResult =
+                sf.joinGame(Double.toString(gameID), ChessGame.TeamColor.WHITE, authToken);
+
+        Assertions.assertEquals("403", joinResult.get("statusCode"));
+    }
+
+    @Test
+    public void joinGameBadAuth() {
+        Map<String, String> joinResult =
+                sf.joinGame(Double.toString(gameID), ChessGame.TeamColor.WHITE, "badAuth");
+
+        Assertions.assertEquals("401", joinResult.get("statusCode"));
     }
 }
