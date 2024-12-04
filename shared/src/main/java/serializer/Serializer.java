07cbce39634db47885a69902c652e839562c1c60
Derek Ashby <dbashby@byu.edu>
2024-12-03 23:10:33 -0700

    Added type adapters to serializer

--- shared/src/main/java/serializer/Serializer.java
+++ shared/src/main/java/serializer/Serializer.java
@@ -2,12 +2,58 @@ package serializer;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
+import websocket.commands.*;
+import websocket.messages.*;
 
 public class Serializer {
+    public static class UserGameCommandDeserializer implements JsonDeserializer<UserGameCommand> {
+        @Override
+        public UserGameCommand deserialize(JsonElement json, Type typeOfT,
+                JsonDeserializationContext context) {
+            JsonObject jsonObject = json.getAsJsonObject();
+            String type = jsonObject.get("commandType").getAsString();
+
+            switch (type) {
+                case ("CONNECT"):
+                    return context.deserialize(jsonObject, ConnectCommand.class);
+                case ("MAKE_MOVE"):
+                    // command = context.deserialize(jsonObject, MakeMoveCommand.class);
+                case ("LEAVE"):
+                    // command = context.deserialize(jsonObject, LeaveCommand.class);
+                case ("RESIGN"):
+                    // command = context.deserialize(jsonObject, ResignCommand.class);
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public static class ServerMessageDeserializer implements JsonDeserializer<ServerMessage> {
+        @Override
+        public ServerMessage deserialize(JsonElement json, Type typeOfT,
+                JsonDeserializationContext context) {
+            var jsonObject = json.getAsJsonObject();
+            var type = jsonObject.get("serverMessageType").getAsString();
+
+            switch (type) {
+                case ("ERROR"):
+                    return context.deserialize(jsonObject, ErrorMessage.class);
+                case ("LOAD_GAME"):
+                    return context.deserialize(jsonObject, LoadGameMessage.class);
+                case ("NOTIFICATION"):
+                    return context.deserialize(jsonObject, NotificationMessage.class);
+                default:
+                    return null;
+            }
+        }
+    }
+
     private final Gson gson;
 
     public Serializer() {
-        this.gson = new Gson();
+        this.gson = new GsonBuilder()
+                .registerTypeAdapter(UserGameCommand.class, new UserGameCommandDeserializer())
+                .registerTypeAdapter(ServerMessage.class, new ServerMessageDeserializer()).create();
     }
 
     public String toJson(Object obj) {
