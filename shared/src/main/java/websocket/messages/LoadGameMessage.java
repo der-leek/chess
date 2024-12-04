9115317d2af871cb7668db4b21b7497d7fa32539
Derek Ashby <dbashby@byu.edu>
2024-12-03 23:11:21 -0700

    Created child ServerMessages

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,23 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class LoadGameMessage extends ServerMessage {
+
+    private final ChessGame game;
+    private final ChessGame.TeamColor teamColor;
+
+    public LoadGameMessage(ChessGame game, ChessGame.TeamColor teamColor) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+        this.teamColor = teamColor;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public ChessGame.TeamColor getTeamColor() {
+        return teamColor;
+    }
+}
